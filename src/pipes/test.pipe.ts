00bce42e72a9aaf2406dd85913dbb92a1e04af93
Desouki <mahmoud.desouki@cibeg.com>
2025-06-15 12:14:08 +0300

    finish test commit

--- src/pipes/test.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
-
-@Injectable()
-export class TestPipe implements PipeTransform<string, string> {
-  transform(value: string, metadata: ArgumentMetadata): string {
-    if (!value) return value;
-    return value.toLowerCase();
-  }
-}
